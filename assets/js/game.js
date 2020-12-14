75308c492a491f4199e1a1b6b4acb7c96154dc81
Kris Pennimpede <kpennimpede@hotmail.com>
2020-12-14 12:49:12 -0500

    testing some basic javascript commands

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! Javascript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// THis will do math and log 20
+console.log(10 + 10);
+
+// What is this?
+console.log("Our Robots name is " + playerName);
+
+// THis creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+// fight()
